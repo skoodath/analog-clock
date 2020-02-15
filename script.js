b4f9da5d4a70aac973c91b938f1cb71fa881e46d
Shiju Nambiar <skoodath@gmail.com>
2020-02-15 23:02:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+setInterval(startClock, 1000);
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+console.log(hourHand, minuteHand, secondHand)
+
+function startClock () {
+    
+    const currentDate = new Date();
+    const secondrate = currentDate.getSeconds() / 60;
+    const minuterate = (secondrate + currentDate.getMinutes()) / 60;
+    const hourrate = (minuterate + currentDate.getHours()) / 12;
+
+
+    rotateHands(hourHand, hourrate);
+    rotateHands(minuteHand, minuterate);
+    rotateHands(secondHand, secondrate);
+}
+
+function rotateHands (elem, rotateratio) {
+    elem.style.setProperty('--rotation', rotateratio * 360);
+}
+startClock();
